a38d68fd63e0143661f48e5191fe583128b133f0
shiyasmohd <shiyasjaseena124@gmail.com>
2024-04-01 13:46:00 +0530

    chore: added sample function to createNewEvent

---
+++ src/lib/functions.ts
@@ -0,0 +1,30 @@
+import { ethers } from "ethers";
+import { FACTORY_CONTRACT_ADDRESS } from "./cosnt";
+import FACTORY_ABI from "./abis/Factory.json";
+
+export const createNewEvent = async (
+    eventName: string,
+    eventDesc: string,
+    prizePool: string,
+    duration: string,
+) => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum as ethers.providers.ExternalProvider)
+
+    const contract = new ethers.Contract(FACTORY_CONTRACT_ADDRESS, FACTORY_ABI, provider)
+
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+    const signer = provider.getSigner();
+    console.log({ signer })
+
+    const transaction = await contract.connect(signer).createFundingContract(
+        eventName,
+        eventDesc,
+        prizePool,
+        duration
+    );
+    console.log({ transaction })
+
+    const response = await transaction.wait();
+    return response
+}
