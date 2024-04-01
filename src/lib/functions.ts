fcf63b0466e10193327a6037e7e87066cb347a62
shiyasmohd <shiyasjaseena124@gmail.com>
2024-04-01 13:46:36 +0530

    fix: typo fix

--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -1,5 +1,5 @@
 import { ethers } from "ethers";
-import { FACTORY_CONTRACT_ADDRESS } from "./cosnt";
+import { FACTORY_CONTRACT_ADDRESS } from "./const";
 import FACTORY_ABI from "./abis/Factory.json";
 
 export const createNewEvent = async (
