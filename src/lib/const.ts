09e5bd468b4d1e3b24cccb445f39c1b23e555cc0
shiyasmohd <shiyasjaseena124@gmail.com>
2024-04-03 17:31:46 +0530

    feat: function to get all events

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,1 +1,3 @@
 export const FACTORY_CONTRACT_ADDRESS = "0xEFf4150c8EE4f390B184eEcc8c01828A58a7b479";
+
+export const SUBGRAPH_QUERY_URL = "https://api.studio.thegraph.com/query/47004/quadfund/version/latest"
--- src/lib/functions.ts
+++ src/lib/functions.ts
@@ -1,6 +1,7 @@
 import { ethers } from "ethers";
-import { FACTORY_CONTRACT_ADDRESS } from "./const";
+import { FACTORY_CONTRACT_ADDRESS, SUBGRAPH_QUERY_URL } from "./const";
 import FACTORY_ABI from "./abis/Factory.json";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
 export const createNewEvent = async (
     eventName: string,
@@ -28,3 +29,34 @@ export const createNewEvent = async (
     const response = await transaction.wait();
     return response
 }
+
+const mainnetClient = new ApolloClient({
+    uri: SUBGRAPH_QUERY_URL,
+    cache: new InMemoryCache(),
+});
+export const getAllEvents = async () => {
+
+    const query = `
+    query {
+        quadFundEvents {
+          id
+          owner
+          name
+          description
+          prizePool
+          startTime
+          endTime
+          resultPublished
+        }
+      }
+  `;
+    try {
+
+        const data = await mainnetClient.query({
+            query: gql(query),
+        });
+        return data.data.quadFundEvents;
+    } catch (err) {
+        console.log("Error fetching data: ", err);
+    }
+};
