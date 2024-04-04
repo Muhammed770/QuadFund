c8a11ae634aaf39c9aa1fc290b38b3384aac1379
shiyasmohd <shiyasjaseena124@gmail.com>
2024-04-04 13:26:09 +0530

    refactor: contract updated and feat:getContributionsByProjectId added

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,3 +1,3 @@
-export const FACTORY_CONTRACT_ADDRESS = "0xcCbD348E1E4D232dCe59AcBdb6cf1d2B5e2310B4";
+export const FACTORY_CONTRACT_ADDRESS = "0x4b338BcA6dD35ed22C60E38A8BC6CC426BCa4d86";
 
 export const SUBGRAPH_QUERY_URL = "https://api.studio.thegraph.com/query/47004/quadfund/version/latest"
