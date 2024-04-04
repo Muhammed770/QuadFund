d9c2a50287058efe06b1ab301d539f238905ac5b
shiyasmohd <shiyasjaseena124@gmail.com>
2024-04-04 10:20:20 +0530

    refactor: cotnracts updated

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,3 +1,3 @@
-export const FACTORY_CONTRACT_ADDRESS = "0xbad349061aC064f81EeDD59aD1CA7d93c4222746";
+export const FACTORY_CONTRACT_ADDRESS = "0xcCbD348E1E4D232dCe59AcBdb6cf1d2B5e2310B4";
 
 export const SUBGRAPH_QUERY_URL = "https://api.studio.thegraph.com/query/47004/quadfund/version/latest"
