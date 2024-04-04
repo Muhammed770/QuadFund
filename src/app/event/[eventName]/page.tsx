b71538effb38f8b3964f71bbaf70fef91746d1d6
Ahamed Zain <ahamedzainthappi10@gmail.com>
2024-04-04 23:58:14 +0530

    withdraw fund

--- src/app/event/[eventName]/page.tsx
+++ src/app/event/[eventName]/page.tsx
@@ -34,7 +34,7 @@ const EventPage = ({ params }: { params: { eventName: string } }) => {
     const [eventId, setEventId] = useState<string>(""); // Store fetched id
     const [projects, setProjects] = useState<ProjectListType>([]); // Store fetched projects
     const [projectContributors, setProjectContributors] = useState([])
-    const [topContributors, setTopContributors] = useState<object>()
+    const [topContributors, setTopContributors] = useState([])
 
     const id = queries.get('id') as string;
 
@@ -75,82 +75,6 @@ const EventPage = ({ params }: { params: { eventName: string } }) => {
 
     const slicedAddress = (address:string) => address.slice(0, 4) + "..." + address.slice(-4);
 
-    const cardData = [
-        {
-            title: "Project A",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            src: "https://images.unsplash.com/photo-1711139299064-f60e2753163f?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            images: ["https://images.unsplash.com/photo-1711139299064-f60e2753163f?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                "https://images.unsplash.com/photo-1707344088547-3cf7cea5ca49?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                "https://images.unsplash.com/photo-1710975090677-5955bce9d325?q=80&w=2651&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                "https://images.unsplash.com/photo-1682687220742-aba13b6e50ba?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                "https://images.unsplash.com/photo-1708649290066-5f617003b93f?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"],
-            link: "#",
-            contributors: "10 Contributors • $100K Matched",
-            owner: "@zain",
-            projectContributorsData: [
-                { username: "@zain", amount: "$5k", src: "https://ui.shadcn.com/avatars/04.png/?height=24&width=24" },
-                { username: "@neda", amount: "$$3.77k", src: "https://ui.shadcn.com/avatars/01.png/?height=24&width=24" },
-                { username: "@shiyas", amount: "$2.87k", src: "https://ui.shadcn.com/avatars/02.png/?height=24&width=24" },
-                { username: "@muhammed770", amount: "$1.57k", src: "https://ui.shadcn.com/avatars/04.png/?height=24&width=24" }
-            ]
-        },
-        {
-            title: "Project B",
-            description: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            contributors: "20 Contributors • $200K Matched",
-            owner: "@zain",
-            src: "https://images.unsplash.com/photo-1707344088547-3cf7cea5ca49?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            images: [],
-            link: "#",
-            projectContributorsData: []
-        },
-        {
-            title: "Project C",
-            description: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
-            about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            contributors: "30 Contributors • $300K Matched",
-            owner: "@zain",
-            src: "https://images.unsplash.com/photo-1710975090677-5955bce9d325?q=80&w=2651&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            images: [],
-            link: "#",
-            projectContributorsData: []
-        },
-        {
-            title: "Project D",
-            description: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-            about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            contributors: "40 Contributors • $400K Matched",
-            owner: "@zain",
-            src: "https://images.unsplash.com/photo-1682687220742-aba13b6e50ba?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            images: [],
-            link: "#",
-            projectContributorsData: []
-        },
-        {
-            title: "Project E",
-            description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-            contributors: "50 Contributors • $500K Matched",
-            owner: "@zain",
-            src: "https://images.unsplash.com/photo-1708649290066-5f617003b93f?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-            images: [],
-            link: "#",
-            projectContributorsData: []
-        }
-    ];
-
-    const contributorsData = [
-        { username: "@zain", amount: "$5k", src: "https://ui.shadcn.com/avatars/04.png/?height=24&width=24" },
-        { username: "@neda", amount: "$3.77k", src: "https://ui.shadcn.com/avatars/01.png/?height=24&width=24" },
-        { username: "@shiyas", amount: "$2.87k", src: "https://ui.shadcn.com/avatars/02.png/?height=24&width=24" },
-        { username: "@muhammed770", amount: "$1.57k", src: "https://ui.shadcn.com/avatars/04.png/?height=24&width=24" }
-    ];
-    // if (!id || !name) {
-    //     return <div>Loading...</div>;
-    // }
     return (
         <div className="flex flex-col lg:flex-row gap-8 md:p-8 p-4">
             <div className="flex-1 space-y-6">
@@ -235,13 +159,6 @@ const EventPage = ({ params }: { params: { eventName: string } }) => {
                                         <TabsTrigger value="owner">Owner</TabsTrigger>
                                     </TabsList>
                                     <TabsContent value="about" className="h-[330px] overflow-scroll no-scrollbar">
-                                        {/* <div className="flex items-center overflow-x-auto no-scrollbar ">
-                                            {data.images.map((image, index) => (
-                                                <div key={index} className="mx-1">
-                                                    <img className="max-w-[400px] h-[200px] rounded-lg" src={image} alt={data.title} />
-                                                </div>
-                                            ))}
-                                        </div> */}
                                         <div className="mt-4 p-3">
                                             <h2 className="text-xl font-semibold">About</h2>
                                             <p className="text-lg pt-2">{data.about}</p>
@@ -289,13 +206,6 @@ const EventPage = ({ params }: { params: { eventName: string } }) => {
                                         </div>
                                     </TabsContent>
                                 </Tabs>
-
-                                {/* <DrawerFooter>
-                          <Button>Submit</Button>
-                          <DrawerClose>
-                            <Button variant="outline">Cancel</Button>
-                          </DrawerClose>
-                        </DrawerFooter> */}
                             </DrawerContent>
 
                         </Drawer>
@@ -314,13 +224,13 @@ const EventPage = ({ params }: { params: { eventName: string } }) => {
                 <div>
                     <h2 className="text-xl font-semibold">Top Contributors</h2>
                     <div className="space-y-2 mt-2">
-                        {contributorsData.map((data, index) => (
+                        {(topContributors as { user: { id: string }, amount: number }[]).map((data, index) => (
                             <div key={index} className="flex items-center space-x-2">
                                 <Avatar>
-                                    <AvatarImage alt={data.username} src={data.src} />
+                                    <AvatarImage alt="Avatar" src="https://ui.shadcn.com/avatars/04.png/?height=24&width=24" />
                                 </Avatar>
-                                <span className="font-medium">{data.username}</span>
-                                <span className="ml-auto">{weiToUSD(data.amount)}</span>
+                                <span className="font-medium">{slicedAddress(data.user?.id)}</span>
+                                <span className="ml-auto">${weiToUSD(data.amount.toString())}</span>
                             </div>
                         ))}
                         <Button className="w-full mt-2" variant="outline">
