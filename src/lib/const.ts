73de584d961560024235ce771300298eee965cd5
muhammed770 <mail.muhammed2002@gmail.com>
2024-05-04 17:15:23 +0530

    fix:updated graphql url

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,3 +1,3 @@
 export const FACTORY_CONTRACT_ADDRESS = "0x623189d62c53a8652e2ff8d16Df4421384308D65";
 
-export const SUBGRAPH_QUERY_URL = "https://api.studio.thegraph.com/query/47004/quadfund/version/latest"
+export const SUBGRAPH_QUERY_URL = "https://api.thegraph.com/subgraphs/id/QmWS4zEons8BEkDj8AktkGmLnBhoiUar6McmviczrjD1EV/graphql"
