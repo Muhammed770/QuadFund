60d43befe194774d6044617e510be0138b24ae52
muhammed770 <mail.muhammed2002@gmail.com>
2024-05-04 17:09:25 +0530

    feat:address and grapg url updated

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,3 +1,3 @@
-export const FACTORY_CONTRACT_ADDRESS = "0x4b338BcA6dD35ed22C60E38A8BC6CC426BCa4d86";
+export const FACTORY_CONTRACT_ADDRESS = "0x623189d62c53a8652e2ff8d16Df4421384308D65";
 
 export const SUBGRAPH_QUERY_URL = "https://api.studio.thegraph.com/query/47004/quadfund/version/latest"
