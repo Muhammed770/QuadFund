0379aee933aef914eb9f05fae6756d56f3e26707
muhammed770 <mail.muhammed2002@gmail.com>
2024-05-05 13:00:03 +0530

    update:contract address

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,3 +1,3 @@
-export const FACTORY_CONTRACT_ADDRESS = "0x623189d62c53a8652e2ff8d16Df4421384308D65";
+export const FACTORY_CONTRACT_ADDRESS = "0xdf710Aa8a0D177890fa5f73235C99100c802bAfc";
 
 export const SUBGRAPH_QUERY_URL = "https://api.thegraph.com/subgraphs/id/QmWS4zEons8BEkDj8AktkGmLnBhoiUar6McmviczrjD1EV"
