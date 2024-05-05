25b4cd18d8c995e900e9ddca9608a3d8bee7c544
muhammed770 <mail.muhammed2002@gmail.com>
2024-05-05 14:28:35 +0530

    feat:graph url updated

--- src/lib/const.ts
+++ src/lib/const.ts
@@ -1,3 +1,3 @@
 export const FACTORY_CONTRACT_ADDRESS = "0xdf710Aa8a0D177890fa5f73235C99100c802bAfc";
 
-export const SUBGRAPH_QUERY_URL = "https://api.thegraph.com/subgraphs/id/QmWS4zEons8BEkDj8AktkGmLnBhoiUar6McmviczrjD1EV"
+export const SUBGRAPH_QUERY_URL = "https://api.thegraph.com/subgraphs/id/QmZCqBKMMKykAzF6Yu47eSkLqBKYryfJJSpXMZG3iHR3CX"
